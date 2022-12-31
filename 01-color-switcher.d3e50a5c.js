425e9d48855680938b38f1d1a28c4cf1d0eb6e86
TaniaShmigel <110722840+TaniaShmigel@users.noreply.github.com>
2022-12-31 15:38:12 +0000

    Deploying to gh-pages from @ TaniaShmigel/goit-js-hw-09@f23a79a02d56d082e84209852f697029834b8c23 🚀

--- 01-color-switcher.d3e50a5c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");function d(){let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=e}e.addEventListener("click",(function(){setInterval(d,1e3),e.setAttribute("disabled","disabled")})),t.addEventListener("click",(function(){let o=setInterval(d,1e3);clearInterval(o),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled"),console.log(o)})),e.removeAttribute("disabled"),t.removeAttribute("disabled");
-//# sourceMappingURL=01-color-switcher.d3e50a5c.js.map
